83bb13a1ca0806f6f4531173ddfc551e17d90162
HyeonwooNoh <hyeonwoonoh@gmail.com>
2015-03-01 21:20:21 +0900

    unpooling registry in layer_factory.cpp

--- src/caffe/layer_factory.cpp
+++ src/caffe/layer_factory.cpp
@@ -61,6 +61,23 @@ Layer<Dtype>* GetPoolingLayer(const LayerParameter& param) {
 
 REGISTER_LAYER_CREATOR(POOLING, GetPoolingLayer);
 
+// Get unpooling layer according to engine.
+template <typename Dtype>
+Layer<Dtype>* GetUnpoolingLayer(const LayerParameter& param) {
+  UnpoolingParameter_Engine engine = param.unpooling_param().engine();
+  if (engine == UnpoolingParameter_Engine_DEFAULT) {
+    engine = UnpoolingParameter_Engine_CAFFE;
+  }
+  if (engine == UnpoolingParameter_Engine_CAFFE) {
+    return new UnpoolingLayer<Dtype>(param);
+  } else {
+    LOG(FATAL) << "Layer " << param.name() << " has unknown engine.";
+  }
+}
+
+REGISTER_LAYER_CREATOR(UNPOOLING, GetUnpoolingLayer);
+
+
 // Get relu layer according to engine.
 template <typename Dtype>
 Layer<Dtype>* GetReLULayer(const LayerParameter& param) {
