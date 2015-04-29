a781788617f5e18e4b9c3c5563f25288ee79d029
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2015-04-29 20:55:30 +0900

    binary accuracy layer is implemented

--- src/caffe/layers/binary_accuracy.cpp
+++ src/caffe/layers/binary_accuracy.cpp
@@ -11,51 +11,37 @@
 namespace caffe {
 
 template <typename Dtype>
-void AccuracyLayer<Dtype>::LayerSetUp(
+void BinaryAccuracyLayer<Dtype>::LayerSetUp(
   const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-  top_k_ = this->layer_param_.accuracy_param().top_k();
 }
 
 template <typename Dtype>
-void AccuracyLayer<Dtype>::Reshape(
+void BinaryAccuracyLayer<Dtype>::Reshape(
   const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   CHECK_EQ(bottom[0]->num(), bottom[1]->num())
       << "The data and label should have the same number.";
-  CHECK_LE(top_k_, bottom[0]->count() / bottom[0]->num())
-      << "top_k must be less than or equal to the number of classes.";
-  CHECK_EQ(bottom[1]->channels(), 1);
-  CHECK_EQ(bottom[1]->height(), 1);
-  CHECK_EQ(bottom[1]->width(), 1);
+  CHECK_EQ(bottom[0]->channels(), bottom[1]->channels())
+      << "The data and label should have the same channels.";
+  CHECK_EQ(bottom[0]->height(), bottom[1]->height())
+      << "The data and label should have the same height.";
+  CHECK_EQ(bottom[0]->width(), bottom[1]->width())
+      << "The data and label should have the same width.";
   top[0]->Reshape(1, 1, 1, 1);
 }
 
 template <typename Dtype>
-void AccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+void BinaryAccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   Dtype accuracy = 0;
   const Dtype* bottom_data = bottom[0]->cpu_data();
   const Dtype* bottom_label = bottom[1]->cpu_data();
-  int num = bottom[0]->num();
-  int dim = bottom[0]->count() / bottom[0]->num();
-  vector<Dtype> maxval(top_k_+1);
-  vector<int> max_id(top_k_+1);
+  int num = bottom[0]->count();
+
   for (int i = 0; i < num; ++i) {
-    // Top-k accuracy
-    std::vector<std::pair<Dtype, int> > bottom_data_vector;
-    for (int j = 0; j < dim; ++j) {
-      bottom_data_vector.push_back(
-          std::make_pair(bottom_data[i * dim + j], j));
-    }
-    std::partial_sort(
-        bottom_data_vector.begin(), bottom_data_vector.begin() + top_k_,
-        bottom_data_vector.end(), std::greater<std::pair<Dtype, int> >());
-    // check if true label is in top k predictions
-    for (int k = 0; k < top_k_; k++) {
-      if (bottom_data_vector[k].second == static_cast<int>(bottom_label[i])) {
-        ++accuracy;
-        break;
-      }
-    }
+    int prediction = static_cast<int>(bottom_data[i] > 0);
+    int label = static_cast<int>(bottom_label[i]);
+    int match = static_cast<int>(prediction == label);
+    accuracy += match;
   }
 
   // LOG(INFO) << "Accuracy: " << accuracy;
@@ -63,6 +49,6 @@ void AccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   // Accuracy layer should not be used as a loss function.
 }
 
-INSTANTIATE_CLASS(AccuracyLayer);
-REGISTER_LAYER_CLASS(ACCURACY, AccuracyLayer);
+INSTANTIATE_CLASS(BinaryAccuracyLayer);
+REGISTER_LAYER_CLASS(BIN_ACCURACY, BinaryAccuracyLayer);
 }  // namespace caffe
